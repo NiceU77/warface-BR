2450f3d47acde9d354c0ae0f0bb282bfd8d9b42e
DGHack <60447535+DGHack@users.noreply.github.com>
2020-01-29 19:00:44 -0300

    Add files via upload

---
+++ signtool/aa.bat
@@ -0,0 +1,6 @@
+cd D:\Users\pc\Desktop\warfa\Unknowncheats.me Onra2 wf minihack\Debug\Bin
+date 1-1-2014
+signtool.exe sign /a /ac "VeriSign Class 3 Public Primary Certification Authority - G5.cer" /f "cert.pfx" /p "1" warface.dll
+date 29-1-2020
+echo "ok"
+pause
